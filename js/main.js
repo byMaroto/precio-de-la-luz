55aaf786faa16f134b678d8294a5c956b74559c5
Alejandro Maroto <alejandromaroto96@gmail.com>
2022-05-04 13:53:26 +0200

    Llamada a la API cada 5 minutos. Recogida de datos. Inicio de la función para las operaciones.

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Variables
+var day = new Date();
+var currentTime = day.getHours() + ':' + day.getMinutes() + ':' + day.getSeconds();
+
+// Price calculation function. Receives the price and consumption.
+function priceCalculation(price, consumption) {
+    // Price / Consumption
+    const result = price/consumption;
+    console.log(result + ' €/hour');
+}
+
+// Fetch function code
+async function fetchApiCall() {
+    try {
+        const response = await fetch('https://api.allorigins.win/get?url=https://api.preciodelaluz.org/v1/prices/all?zone=PCB');
+
+        if(response.ok){
+            const data = await response.json();
+            console.log(data);
+
+            const contentsResponse = await fetch(data.contents);
+            console.log(contentsResponse);
+
+            const contents = await contentsResponse.json();
+            console.log(contents);
+
+            // LocalStorage code
+            localStorage.setItem("data", JSON.stringify(data));
+            console.log(localStorage.getItem("data"));
+
+            priceCalculation('500', '1000');
+        } else {
+            console.log("Error");
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+// Call API every 5 minutes
+//window.setInterval(function () {
+    fetchApiCall();
+//}, 300000);
