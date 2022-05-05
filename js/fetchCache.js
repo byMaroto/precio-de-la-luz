77c600f9eb6b8acd7a67c574ca8ec6e5caeb068f
Alejandro Maroto <alejandromaroto96@gmail.com>
2022-05-05 20:26:25 +0200

    Precios y horas obtenidos.

--- js/fetchCache.js
+++ js/fetchCache.js
@@ -15,16 +15,24 @@ function priceCalculation(price, consumption) {
 async function fetchApiCall() {
     try {
         const response = await fetch('https://api.allorigins.win/get?url=https://api.preciodelaluz.org/v1/prices/all?zone=PCB');
+        const prices = [];
+        const hours = [];
 
         if(response.ok){
             const dataJSON = await response.json();
             const data = JSON.parse(dataJSON.contents);
             console.log(data);
 
-            for (let i = 0; i < 10; i++) {
-                const price = data[1].price;
-                console.log(price);
+            // For to get the Price and Hour and assign to the Arrays
+            for (let i in data) {
+                if (data[i].hasOwnProperty('price')) {
+                    prices[i] = data[i].price;
+                    hours[i] = data[i].hour;
+                }
             }
+            console.log(prices);
+            console.log(hours);
+              
 
             // LocalStorage code
             //localStorage.setItem("data", JSON.stringify(data));
